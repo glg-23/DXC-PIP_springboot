f7e4f5ee41bd9d22287229983e15e0fc91e7145f
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-14 17:03:51 +0200

    rimosso welcome page da HelloController

--- DXC_PIP_SpringBootApp/src/main/java/com/DXC_PIP/controller/HelloController.java
+++ DXC_PIP_SpringBootApp/src/main/java/com/DXC_PIP/controller/HelloController.java
@@ -1,22 +1,21 @@
 package com.DXC_PIP.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
     @GetMapping(value = "/")
     public String index() {
         return "Pagina principale";
-    }
-
-    @GetMapping(value = "/welcome")
-    public String welcome() {
-        return "Benvenuto in Spring Boot";
     }
 
     @GetMapping(value = "/hello")
     public String ciao() {
         return "Ciao";
     }
+
 }
