1cb7db76230d13b3745b910da4b09647f127422b
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-14 17:04:43 +0200

    init welcome welcome page - controller + html

---
+++ DXC_PIP_SpringBootApp/src/main/java/com/DXC_PIP/controller/WelcomeController.java
@@ -0,0 +1,18 @@
+package com.DXC_PIP.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping(value = "/welcome")
+    public String welcome(@RequestParam String name, Model model) {
+        String messaggio = "Benvenuto " + name;
+        model.addAttribute("messaggioBenvenuto", messaggio);
+        return "welcomePage";
+    }
+
+}
