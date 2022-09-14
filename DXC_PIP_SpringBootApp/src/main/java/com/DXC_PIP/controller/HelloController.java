b03196a05dc0f0e9e1fe5dcda2516a01d785173e
gdantimi <gianluigi.dantimi@gmail.com>
2022-09-14 15:31:45 +0200

    Update HelloController.java

--- DXC_PIP_SpringBootApp/src/main/java/com/DXC_PIP/controller/HelloController.java
+++ DXC_PIP_SpringBootApp/src/main/java/com/DXC_PIP/controller/HelloController.java
@@ -1,22 +1,21 @@
 package com.DXC_PIP.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping(value = "/")
     public String index() {
         return "Pagina principale";
     }
 
-    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    @GetMapping(value = "/welcome")
     public String welcome() {
         return "Benvenuto in Spring Boot";
     }
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping(value = "/hello")
     public String ciao() {
         return "Ciao";
     }
